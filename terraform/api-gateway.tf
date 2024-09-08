8779d4a671c2f87f1204f0dca619ab9214641bc1
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-09-08 14:13:02 -0300

    error: 403 to create api gateway

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,25 +1,25 @@
-// resource "aws_api_gateway_rest_api" "ti-help-dirver-gateway" {
-//  name = "ti-help-dirver-gateway"
-//
-//  body = <<EOF
-//{
-//    openapi = "3.0.1"
-//    info = {
-//      title   = "example"
-//      version = "1.0"
-//    }
-//    paths = {
-//      "/path1" = {
-//        get = {
-//          x-amazon-apigateway-integration = {
-//            httpMethod           = "GET"
-//            payloadFormatVersion = "1.0"
-//            type                 = "HTTP_PROXY"
-//            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
-//          }
-//        }
-//      }
-//    }
-//}
-//EOF
-//}
+resource "aws_api_gateway_rest_api" "ti-help-dirver-gateway" {
+  name = "ti-help-dirver-gateway"
+
+  body = <<EOF
+{
+    openapi = "3.0.1"
+    info = {
+    title   = "example"
+    version = "1.0"
+  }
+  paths = {
+    "/path1" = {
+      get = {
+        x-amazon-apigateway-integration = {
+          httpMethod           = "GET"
+          payloadFormatVersion = "1.0"
+          type                 = "HTTP_PROXY"
+          uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+          }
+        }
+      }
+    }
+}
+EOF
+}
