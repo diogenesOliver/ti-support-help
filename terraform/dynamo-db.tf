4a4e0d6227c43c3a8c457a5727ee5504c1ad5427
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-10-02 18:03:06 -0300

    feat: removing on attribute

--- terraform/dynamo-db.tf
+++ terraform/dynamo-db.tf
@@ -2,18 +2,12 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "MessageSupport"
   billing_mode   = "PROVISIONED"
   hash_key       = "TicketSupport"
-  range_key      = "MessageID"
 
   read_capacity  = 20
   write_capacity = 20
 
   attribute {
     name = "TicketSupport"
-    type = "S"
-  }
-
-  attribute {
-    name = "MessageID"
     type = "S"
   }
 }
