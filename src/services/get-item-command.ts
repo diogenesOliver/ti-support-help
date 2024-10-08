eb060c1332469183d42e7a6b3370069635fcf463
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-10-07 23:10:01 -0300

    wip: creating function to get item

---
+++ src/services/get-item-command.ts
@@ -0,0 +1,6 @@
+import { dynamoDBClientInstance } from "../lib/dynamo-db";
+import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+
+export async function getMessages(){
+   let docClient: string;
+}
