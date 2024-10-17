31217309e427cbaacf1e9052cbdd7f6417466b48
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-10-16 21:56:06 -0300

    fix: fixing sintax error of cloudwatch module

--- terraform/cloudwatch-log-sqs.tf
+++ terraform/cloudwatch-log-sqs.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "sqs_log_group" {
-  name              = "/aws/sqs/my-queue"
-  retention_in_days = 7
-}
-
 resource "aws_cloudwatch_metric_alarm" "tickets-pending" {
   alarm_name          = "SQSVisibleMessagesAlarm"
   comparison_operator = "GreaterThanThreshold"
@@ -16,13 +11,11 @@ resource "aws_cloudwatch_metric_alarm" "tickets-pending" {
 
   alarm_description = "Alarme quando a fila tiver mais de 10 mensagens visíveis"
 
-  actions_enabled = true
-  alarm_actions   = [aws_sns_topic.sqs_alarm_topic.arn]
   dimensions = {
     QueueName = aws_sqs_queue.ti-help-driver-sqs.name
   }
 }
 
-resource "aws_sns_topic" "sqs_alarm_topic" {
+resource "aws_sqs_queue" "sqs_alarm_topic" {
   name = "sqs-alarm-topic"
 }
