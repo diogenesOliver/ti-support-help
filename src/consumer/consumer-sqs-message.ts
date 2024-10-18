7081c04ff7e1fc18d97a3198af7a26d35db5dfd4
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-10-17 22:03:16 -0300

    chore: separating on two conditionals

--- src/consumer/consumer-sqs-message.ts
+++ src/consumer/consumer-sqs-message.ts
@@ -10,13 +10,17 @@ export const consumerMessage = async (app: FastifyInstance) => {
     app.get('/tickets', async (request, reply) => {
         const getTicketDataFromDynamoDB = await getMessages()
 
-        if(getTicketDataFromDynamoDB?.length === 0)
+        if (getTicketDataFromDynamoDB?.length === 0)
             return reply.status(100).send({ message: "Messages not found" })
 
         const sqsDDL = (await listDeadLetterSourceQueues()).queueUrls
-        if(sqsDDL === undefined || sqsDDL?.length > 0)            
-            return reply.status(101).send( { message: "MESSAGE PROBLEM", info: sqsDDL } )
+        
+        if (sqsDDL === undefined)
+            return reply.status(101).send({ message: "Property is undefined" })
 
-        return reply.status(200).send( getTicketDataFromDynamoDB )
+        if (sqsDDL?.length > 0)
+            return reply.status(101).send({ message: "message problem", info: sqsDDL })
+
+        return reply.status(200).send(getTicketDataFromDynamoDB)
     })
 }
