d87b4a4e08aff1a9c282076a22bd593d3fb5d2eb
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-11-08 19:30:19 -0300

    style: changing identation

--- terraform/dynamo-db.tf
+++ terraform/dynamo-db.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "MessageSupport"
-  billing_mode   = "PROVISIONED"
-  hash_key       = "TicketSupport"
+  name         = "MessageSupport"
+  billing_mode = "PROVISIONED"
+  hash_key     = "TicketSupport"
 
   read_capacity  = 20
   write_capacity = 20
