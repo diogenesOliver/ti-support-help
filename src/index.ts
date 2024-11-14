b13385dbda1380801a48f90f5528ede017b5e07e
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-11-14 15:40:27 -0300

    feat: adding upload logs file feature on ulr endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { urlencoded } from 'body-parser'
 
 import { openSupportTicket } from './services/open-support-ticket'
 import { consumerMessage } from './consumer/consumer-sqs-message'
+import { uploadLogsFileToS3 } from "./lambda/send-logs-s3";
+
+import path from "path"
 
 const app: FastifyInstance = fastify()
 
@@ -14,5 +17,6 @@ app.register(fastifyExpress)
 
 app.register(openSupportTicket)
 app.register(consumerMessage)
+app.register(uploadLogsFileToS3)
 
 export = { app }
--- src/lambda/send-logs-s3.ts
+++ src/lambda/send-logs-s3.ts
@@ -3,24 +3,30 @@ import { bucketS3Instance } from "../lib/bucket-s3";
 
 import fs from "fs"
 import path from "path"
+import { FastifyInstance } from "fastify";
 
-async function uploadLogsFileToS3(buckerName: string, filePath: string, key: string) {
-    const fileContent = fs.readFileSync(filePath)
+export const uploadLogsFileToS3 = async (app:FastifyInstance) => {
+    app.get('/ngrok/send/logs', async (request, reply) => {
+        
+        async function uploadLogs(buckerName: string, filePath: string, key: string){
+            const fileContent = fs.readFileSync(filePath)
 
-    const uploadParams = {
-        Bucket: buckerName,
-        Key: key,
-        Body: fileContent
-    }
+            const uploadParams = {
+                Bucket: buckerName,
+                Key: key,
+                Body: fileContent
+            }
+        
+            const command = new PutObjectCommand(uploadParams)
+            const reponse = await bucketS3Instance.send(command)
+        
+            return reply.status(201).send({
+                message: "Upload_Logs_File_Succes",
+                reponse
+            })
+        }
 
-    const command = new PutObjectCommand(uploadParams)
-    const reponse = await bucketS3Instance.send(command)
-
-    return console.log({
-        message: "Upload_Logs_File_Succes",
-        reponse
+        const joinPath = path.join(__dirname, "../../ngrok_debug.log")
+        uploadLogs("bucket-logs", joinPath, "uploads/logs/ngrok-logs.txt")
     })
 }
-
-const filePath = path.join(__dirname, "../../ngrok_debug.log")
-uploadLogsFileToS3("bucket-logs", filePath, "uploads/logs/ngrok-logs.txt")
