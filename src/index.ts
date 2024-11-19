4957c35ac9780b983dad06eb936b50b848c1a9f6
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-11-19 19:56:02 -0300

    feat: configuring morgan

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import fastify, { FastifyInstance } from "fastify";
 import fastifyExpress from '@fastify/express'
+import morgan from 'morgan'
 import { urlencoded } from 'body-parser'
 
 import { openSupportTicket } from './services/open-support-ticket'
 import { consumerMessage } from './consumer/consumer-sqs-message'
 import { uploadLogsFileToS3 } from "./lambda/send-logs-s3";
 
-import path from "path"
+import { apiLogs } from "./config/morgan-logs";
 
 const app: FastifyInstance = fastify()
 
 app.register(fastifyExpress)
     .after(() => {
         app.use(urlencoded({ extended: true }))
+        app.use(morgan('combined', { stream: apiLogs() }))
     })
 
 app.register(openSupportTicket)
