b764ef3d377817cd1b2c389307080f8010de0fe2
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-11-20 18:58:36 -0300

    feat: adding versioning feature on bucket logs

--- terraform/bucket-s3.tf
+++ terraform/bucket-s3.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "bucket-logs" {
   bucket = "bucket-logs"
 }
+
+resource "aws_s3_bucket_versioning" "versioning-bucket-logs" {
+  bucket = aws_s3_bucket.bucket-logs.id
+  versioning_configuration{
+    status = "Enabled"
+  }
+}
