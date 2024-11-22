bd7795992b81efb7d9204eafc78b8656cd297f8c
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-11-21 21:17:46 -0300

    feat: adding lifecycle plicy on bucket logs

--- terraform/bucket-s3.tf
+++ terraform/bucket-s3.tf
@@ -8,3 +8,20 @@ resource "aws_s3_bucket_versioning" "versioning-bucket-logs" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "delete-logs-from-1y" {
+  bucket = aws_s3_bucket.bucket-logs.id
+
+  rule {
+    id = "DeleteLogsFromOneYear"
+    status = "Enabled"
+
+    filter {
+      prefix = "uploads/logs/"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
