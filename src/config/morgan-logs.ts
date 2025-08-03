b31f3490d649c488ebbe770e02fea99c059f365c
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-08-03 14:28:12 -0300

    fix: fixing imports

--- src/config/morgan-logs.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-export function apiLogs() {
-    return fs.createWriteStream(path.join(__dirname, '../../access.log'), { flags: 'a' })
-}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import fastify, { FastifyInstance } from "fastify";
-import { apiLogs } from "./config/morgan-logs";
 
-import { openSupportTicket } from './services/open-support-ticket'
-import { consumerMessage } from './consumer/consumer-sqs-message'
-import { uploadLogsFileToS3 } from "./lambda/send-logs-s3";
+import { openSupportTicket } from './use_cases/open-support-ticket'
+import { consumerMessage } from './use_cases/consumer-sqs-message'
 
 const app: FastifyInstance = fastify()
 
@@ -13,6 +11,5 @@ app.register(
 
 app.register(openSupportTicket)
 app.register(consumerMessage)
-app.register(uploadLogsFileToS3)
 
 export = app
