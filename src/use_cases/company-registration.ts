f758617b19fd0bce815732df07674c3b57cfbbfd
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-08-12 20:57:20 -0300

    wip: configuring producer kafka on company-registration feature

--- src/use_cases/company-registration.ts
+++ src/use_cases/company-registration.ts
@@ -2,12 +2,33 @@ import { config } from "dotenv";
 config()
 
 import { Domain_Company_Data } from "../domain/ApplicationDomainLayer"
+import { saveOnDataBaseInstance } from "../db/SaveOnDataBaseInstance";
+import { KafkaInstance } from "../lib/kafka";
 
 import { FastifyInstance } from "fastify";
 import { z } from "zod"
 
 export async function companyRegistration(app: FastifyInstance){
-    app.post('/company/registration/v1', async (Request, reply) => {
+    app.post('/company/registration/v1', async (request, reply) => {
         const companyDataValidation = z.object(Domain_Company_Data)
+        const companyData = companyDataValidation.parse(request.body)
+
+        try {
+            const producer = KafkaInstance.producer()
+            await producer.connect()
+
+            await producer.send({
+                topic: "topic-test",
+                messages: [
+                    {
+                        value: JSON.stringify(companyData) as string
+                    }
+                ]
+            })
+
+            await producer.disconnect()
+        }catch(e){
+            console.log(`Kafka producer ERROR - ${e}`)
+        }
     })
 }
