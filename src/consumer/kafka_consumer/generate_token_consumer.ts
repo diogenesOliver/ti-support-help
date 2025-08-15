438a931c910f4787f779c99e88a0bd5a1acf5cf0
diogenesRibeiro <diogenes.riboliveira@gmail.com>
2025-08-15 20:17:51 -0300

    wip: creating generate_toke_kafka_consumer function

--- src/consumer/kafka_consumer/generate_token_consumer.ts
+++ src/consumer/kafka_consumer/generate_token_consumer.ts
@@ -1,25 +1,33 @@
-import { KafkaInstance } from "../../lib/kafka";
+import { config } from "dotenv";
+config()
 
-async function generateTokenConsumer(){
+import { KafkaInstance } from "../../lib/kafka";
+import { sign } from "jsonwebtoken"
+
+export async function generateTokenConsumer(){
     const consumer = KafkaInstance.consumer({ groupId: "generateToken-group" })
 
     try{
         await consumer.connect()
         await consumer.subscribe({ topic: "topic-test", fromBeginning: true })
 
+        console.info("✅ CONSUMER INSCRITO NO TÓPICO")
+        const TOKEN: string = token()
+
         await consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
+            eachMessage: async ({ topic, partition, message, heartbeat }) => {
+
                 console.log({
                     value: message.value?.toString()
                 })
+                await heartbeat()
             }
         })
-
-        console.log("MENSAGEM CONSUMIDA COM SUCESSO")
-
     }catch(e){
-
+        console.error(`ERRO - Erro ao consumir as mensagens do Kafka: ${e}`)
     }   
 }
 
-generateTokenConsumer()
+function token(): string{
+    return sign({ foo: "baar" }, String(process.env.SECRET_KEY_FROM_TOKEN))
+}
