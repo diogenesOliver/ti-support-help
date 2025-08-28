79b2ae9dfb5435ef2a2debf630aa2d689971148b
diogenesRibeiro <diogenes.riboliveira@gmail.com>
2025-08-28 16:10:01 -0300

    feat: configuring new id brocker

--- src/lib/kafka.ts
+++ src/lib/kafka.ts
@@ -1,6 +1,13 @@
+import { config } from "dotenv"
+config()
+
 import { Kafka } from "kafkajs"
+
+const KAFKA_BROKER_1: string = `${process.env.KAFKA_BROKER_1}:9092`
 
 export const KafkaInstance = new Kafka({
     clientId: "ti-support-help",
-    brokers: ["localhost:9092"]
+    brokers: [
+        KAFKA_BROKER_1
+    ]
 })
