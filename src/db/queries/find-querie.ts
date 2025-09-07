35c976b207311e8cd2e2dcc2b8a06e91b3b5409a
diogenesRibeiro <diogenes.riboliveira@gmail.com>
2025-09-07 14:27:16 -0300

    feat: creting logic to receive a new params

--- src/db/queries/find-querie.ts
+++ src/db/queries/find-querie.ts
@@ -1,11 +1,21 @@
 import { prismaClient } from "../PrismaInstance"
 
-export async function findQuerie(tableName: string){
+export async function findQuerie(tableName: string, paramInclude?: string | undefined){
     try{
-        const results = await prismaClient[
-            tableName
-        ].findMany()
-        
+        if(paramInclude == undefined){
+            const results = await prismaClient[
+                tableName
+            ].findMany()
+            
+            return results
+        }
+
+        const results = await prismaClient[tableName].findMany({
+                include: {
+                    paramInclude
+                }
+            })
+
         return results
     }catch(e){
         console.error(`[ERROR] - FIND QUERIE ERROR: ${e}`)
