ee6bec224ad7bc4ec427b37ff62a7be1bf743181
diogenesRibeiro <diogenes.riboliveira@gmail.com>
2025-11-17 23:06:05 -0300

    feat: validating params

--- src/db/queries/insert-querie.ts
+++ src/db/queries/insert-querie.ts
@@ -2,18 +2,24 @@ import { prismaClient } from "../PrismaInstance"
 
 type UrlParamIdTyping = string | undefined
 
-export async function insertQuerie(tableName: string, data: object, argumentName: string, paramUrlId?: UrlParamIdTyping): Promise<object | undefined> {
+export async function insertQuerie(tableName: string, data: object, argumentName?: string, paramUrlId?: UrlParamIdTyping): Promise<object | undefined> {
     try{
-        if(paramUrlId == undefined){
+        if(paramUrlId == undefined || argumentName == undefined){
             await prismaClient[tableName].create({
                 data: data
             })
             return data
         }
 
-        const relationData = argumentName === 'company' || argumentName === 'companyId' 
-            ? { company: { connect: { id: paramUrlId } } }
-            : { [argumentName]: paramUrlId }
+        let relationData
+
+        if(argumentName === 'company' || argumentName === 'companyId'){
+            relationData = { company: { connect: { id: paramUrlId } } }
+        }else if(argumentName === 'collaborator' || argumentName === 'collaboratorId'){
+            relationData = { collaborator: { connect: { id: paramUrlId } } }
+        }else{
+            relationData = { [argumentName]: paramUrlId }
+        }
 
         await prismaClient[tableName].create({
             data: {
